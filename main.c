4879a3c6a243baac4fa837a38f525b4ae8b38052
PatrickWas <pwas50@stuy.edu>
2024-12-02 14:45:29 -0500

    added some like header files to main

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
 
 int main(){
   return 0;
