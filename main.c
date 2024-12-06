fc045868abdb45c194af087dde10ae01a676bd1d
PatrickWas <pwas50@stuy.edu>
2024-12-06 11:30:35 -0500

    changes made

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main(){
     // checks the path again and again! Super duper cool
     char path[256];
     if(getcwd(path, sizeof(path)) != NULL){
-      printf("%s: $ ", path);
+      printf("\e[1;38;2;0;200;0m%s:\e[0m $ ", path);
     }
     else{
       perror("error with getting path\n");
@@ -31,6 +31,7 @@ int main(){
       }
       if(strncmp(line,"exit", 4) == 0){
         exit(0);
+      printf("%s: $ ", path);
       }
 
       // extra newline
@@ -38,32 +39,30 @@ int main(){
         line[strlen(line) - 1] = '\0';
       }
 
-      parse_args(line, args);
-      pid_t child = fork();
-      if(child == 0){
-        if(execvp(args[0], args) == -1){
-          perror("something happened :(");
-          exit(1);
+      // execvp() and wait(), as well as parsing for ";" here
+      char lineCop[256];
+      strcpy(lineCop, line);
+      char* lineCopy = lineCop;
+      char* token;
+      while((token = strsep(&lineCopy, ";")) != NULL){
+        printf("%s\n", token);
+        parse_args(token, args);
+        pid_t child = fork();
+        if(child == 0){
+          if(execvp(args[0], args) == -1){
+            perror("something happened :(");
+            exit(1);
+          }
+        } else {
+          int status;
+          waitpid(child, &status, 0);
         }
-      } else {
-        int status;
-        waitpid(child, &status, 0);
       }
-      printf("%s: $ ", path);
+
+      printf("\e[1;38;2;0;200;0m%s:\e[0m: $ ", path);
     }
+  }
     
     return 0;
-  }
-
-  /*while(fgets(line, 256, stdin) != NULL){
-    printf("%s\n", line);
-    parse_args(line, args);
-
-    //pid_t child = fork(); holy crap this is the worst thing I have ever done
-    if(child == -1){perror("I cannot make children"); exit(1);}
-    execvp(args[0], args);
-    printf("%s\n", line);
-  }
-  */
-  return 0;
 }
+
