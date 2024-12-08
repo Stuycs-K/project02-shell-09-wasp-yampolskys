f6579f0621a7d00e9a3f6471f0cb8d42a7961612
PatrickWas <pwas50@stuy.edu>
2024-12-08 18:32:12 -0500

    updated formatting; cd is working and everything looks a little bit better now

--- main.c
+++ main.c
@@ -11,82 +11,104 @@
 //#include "cd.h"
 //
 
+void displayPath();
+char* readstdin();
 void cd(char* path);
+void execComm(char* cmd);
 
 int main(){
-  char* args[256];
-  char line[256];
+  char* line;
+  char* token;
+  
   while(1){
     
-    // checks the path again and again! Super duper cool
-    //
+    displayPath();
+    line = readstdin();
+
+    // checks for end-of-file character/essentially making sure command + D works
     if(feof(stdin)){
       return 0;
     }
 
-    char path[256];
-    if(getcwd(path, sizeof(path)) != NULL){
-      printf("\e[1;38;2;0;200;0m%s:\e[0m $ ", path);
-    }
-    else{
-      perror("error with getting path\n");
-      exit(1);
+    // extra newline
+    if(line[strlen(line)-1] == '\n'){
+      line[strlen(line)-1] = '\0';
     }
 
-    while(fgets(line, 256, stdin)){
-      if(strncmp(line,"exit", 4) == 0){
-        exit(0);
-      }
-      if(strncmp(line,"cd",2) == 0){
-        parse_args(line, args);
-        if(args[1] != NULL){
-          cd(args[1]);
-        }
-        else{
-          chdir(getenv("HOME"));
-        }
-        if(getcwd(path,sizeof(path)) != NULL){
-          printf("\e[1;38;2;0;200;0m%s:\e[0m $ ", path);
-        }
-        continue;
-      }
-
-      // extra newline
-      if (line[strlen(line) - 1] == '\n') {
-        line[strlen(line) - 1] = '\0';
-      }
-
-      // execvp() and wait(), as well as parsing for ";" here
-      char lineCop[256];
-      strcpy(lineCop, line);
-      char* lineCopy = lineCop;
-      char* token;
-      while((token = strsep(&lineCopy, ";")) != NULL){
-        printf("%s\n", token);
-        parse_args(token, args);
-        pid_t child = fork();
-        if(child == 0){
-          if(execvp(args[0], args) == -1){
-            perror("something happened :(");
-            exit(1);
-          }
-        } else {
-          int status;
-          waitpid(child, &status, 0);
-        }
-      }
-
-      printf("\e[1;38;2;0;200;0m%s:\e[0m: $ ", path);
+    char lineCop[256];
+    strcpy(lineCop, line);
+    char* lineCopy = lineCop;
+    while((token = strsep(&lineCopy, ";")) != NULL){
+      execComm(token);
     }
+
   }
-    
+
     return 0;
 }
+
 
 void cd(char* path){
   if(chdir(path) != 0){
     perror("cd not working");
   }
 }
+
+char* readstdin(){
+  char* line = malloc(256);
+  
+  if(fgets(line, 256, stdin) == NULL){
+    free(line);
+    return NULL;
+  }
+
+  return line;
+}
+
+void displayPath(){
+  char cwd[1024];
+  char* shorten = getenv("HOME");
+
+  if(getcwd(cwd, sizeof(cwd)) != NULL){
+    if(strncmp(cwd, shorten, strlen(shorten)) == 0){
+      printf("\e[1;38;2;0;200;0m~%s:\e[0m $ ", cwd+strlen(shorten));
+    }
+    else{
+      printf("\e[1;38;2;0;200;0m%s:\e[0m $ ", cwd);
+    }
+  }
+}
+
+void execComm(char* cmd){
+  char* args[256];
+  parse_args(cmd, args);
+
+  if(strcmp(args[0], "cd") == 0){
+    if(args[1] != NULL){
+      cd(args[1]);
+    }
+    else{
+      chdir(getenv("HOME"));
+    }
+    return;
+  }
+
+  if(strcmp(args[0], "exit") == 0){
+    exit(0);
+  }
+  
+  pid_t child = fork();
+  if(child == 0){
+    if(execvp(args[0], args) == -1){
+      perror("execvp not working? That's quite annoying");
+      exit(1);
+    }
+  }
+  else{
+    wait(NULL);
+  }
+
+}
+
 
 // some bugs: "echo" does not get rid of quotation marks
