5eb514e5704d447723dd8ebd1a68162a2e80d5b0
PatrickWas <pwas50@stuy.edu>
2024-12-08 20:45:28 -0500

    file no longer needed

--- main.h
+++
@@ -1,9 +0,0 @@
-// we can totally change the name of this file later (as well as main.c)
-#ifndef MAIN_H
-#define MAIN_H
-int main();
-void displayPath();
-char* readstdin();
-void cd(char* path);
-void execComm(char* cmd);
-#endif
